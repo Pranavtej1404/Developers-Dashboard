71dd9ffe4b52adbbc58a0e65f96513815cdfbf1a
Pranavtej1404 <npranavtej@gmail.com>
2024-12-09 21:19:09 +0530

    corrected

--- project/app/ProjectDetails/[ProjectId]/page.js
+++ project/app/ProjectDetails/[ProjectId]/page.js
@@ -1,29 +1,19 @@
-
+'use client'
 import React from 'react';
 import styles from '../../styles/ProjectDetails.module.css';
-import { useParams } from 'next/navigation';
+
 import axios from 'axios';
 import { api } from '../../components/api';
 import { useState,useEffect } from 'react';
 import stylese from "../../styles/card.module.css";
 
-export async function generateStaticParams() {
-  
-  
-  
-}
-
-export default function ProjectDetails () {
-  const params=useParams();
-  const {ProjectDetails}=params;
- 
+export default await function generateStaticParams ({params}) {
   const [Projs,setAllProjs]= useState({});
   const [tasks,setTasks]=useState([]);
- 
   useEffect(()=>{
   const fetchdata=async () =>{
     try{
-      const response = await axios.get(`${api}/dashboard/Projectdet/${ProjectDetails}`);
+      const response = await axios.get(`${api}/dashboard/Projectdet/${params.ProjectId}`);
       const response1 = await axios.get(`${api}/dashboard/taskdet`);
       setAllProjs(response.data);
       setTasks(response1.data);
@@ -38,7 +28,8 @@ export default function ProjectDetails () {
   
   return (
     <div className={styles.projectDetails}>
-      <h2>Project Details</h2>
+      
+      <h2>Project Details </h2>
       <h3>{Projs.name}</h3>
       <h2>{Projs.description}</h2>
       <div className={styles.projectSections}>
